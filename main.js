cab5e8afe5b62c0c199d9cb91485d509a07bc16c
taeyong <baeyong1227@gmail.com>
2022-08-14 02:29:30 -0400

    showing ricky how to push/pull!!!!!

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-const raceList = document.querySelector(".raceList");
+const raceList = document.querySelector(".race-List");
 const facts = document.querySelector(".facts");
 
 const api = {
   url: "http://ergast.com/api/f1/2021.json",
-  jpar: ".json",
-  parameter: "?callback=myParser"
-
 };
 
-const { url, jpar, parameter } = api;
+const { url } = api;
 const apiUrl = `${url}`;
 
 console.log(apiUrl);
@@ -26,19 +23,14 @@ fetch(apiUrl)
       let a = document.createElement("p");
       a.setAttribute("target", "_blank");
       a.addEventListener("click", function () { myFunction(race.raceName) });
-      a.setAttribute("style", "padding-top: 3pm;");
+      a.setAttribute("style", "padding-top: 3px;");
       a.textContent = race.raceName;
-      R.className = "raceList";
+      R.className = "race-List";
       R.appendChild(a);
       raceList.appendChild(R);
 
       // do the work here
     })
-    // file1.RaceTable.forEach(
-    //     (race_name) => {
-
-
-
   }
   );
 
